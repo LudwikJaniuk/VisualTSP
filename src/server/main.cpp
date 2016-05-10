508596a7e6a632005261deb70532c4ec14e9b6d8
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-05-10 22:58:20 +0200

    Forgot to add some files

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 #include <boost/asio.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
@@ -10,6 +11,8 @@ using namespace std;
 using boost::asio::ip::tcp;
 using boost::property_tree::ptree;
 using boost::property_tree::read_json;
+
+typedef vector<int> path_t;
 
 int port = 3000;
 
@@ -36,6 +39,7 @@ int main()
 			char readBuf[inputSize] = {0};
 			bytesRead = socket.read_some(boost::asio::buffer(readBuf, inputSize));
 			string msg(readBuf, bytesRead);
+			cout << "Received json:\n===========\n" << msg << "\n==========" << endl;
 
 			msg = msg_from_json(msg);
 
@@ -56,18 +60,46 @@ string make_daytime_string()
 	return ctime(&now);
 }
 
+path_t nodes_from_tree(ptree tree) {
+	path_t nodes;
+	for (auto child: tree) {
+		nodes.push_back(child.second.get<int>("x"));
+	}
+	return nodes;
+}
+
+void tsp_solve(path_t& nodes) {
+	reverse(nodes.begin(), nodes.end());
+}
+
+ptree tree_from_nodes(const path_t& nodes) {
+	ptree nodes_tree;
+	for (auto x: nodes) {
+		ptree node;
+		node.put("x", x);
+		node.put("y", 2);
+		node.put("z", 3);
+		nodes_tree.push_back(make_pair("", node)); 
+	}
+	return nodes_tree;
+}
+
 string msg_from_json(string json) 
 {
 	ptree pt;
 	istringstream is(json);
 	read_json(is, pt);
-	string msg = pt.get<string>("message");
-	int rep = pt.get<int>("repeat_times");
-	string result;
-	for (int i = 0; i < rep; i++) 
-	{
-		result.append(msg);
-	}
-	return result;
+	ptree nodes_tree = pt.get_child("nodes");
+	path_t nodes = nodes_from_tree(nodes_tree);
+
+	tsp_solve(nodes);
+
+	pt.clear();
+	pt.put_child("nodes", tree_from_nodes(nodes));
+
+	ostringstream buf;
+	write_json(buf, pt, false); 
+
+	return buf.str();
 }
 
