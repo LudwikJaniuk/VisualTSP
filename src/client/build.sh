a59ddc9e9659fa2161acfa16ec86013dabc203ea
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-05-20 13:46:00 +0200

    QT QT QT

--- src/client/build.sh
+++ src/client/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -std=c++11 -I /usr/include/boost -I ../common main.cpp ../common/common.cpp -o client -lboost_system -lboost_thread -lpthread
+g++ -std=c++11 -I /usr/include/boost -I ../common main.cpp ../common/common.cpp -o client.bin -lboost_system -lboost_thread -lpthread
--- src/run.sh
+++ src/run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-./server/server& ./client/client "127.0.0.1"
+./server/server& ./client/client.bin "127.0.0.1"
