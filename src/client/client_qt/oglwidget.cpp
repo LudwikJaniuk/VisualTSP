ed0ba563eb07ddada63f4716649f4ae906ff3bfc
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-05-23 21:42:06 +0200

    Perspective fixed to be good.
    That means, at least everything between (-1, -1, -1) and (1, 1, 1) should
    be rendered.

--- src/client/client_qt/oglwidget.cpp
+++ src/client/client_qt/oglwidget.cpp
@@ -21,24 +21,37 @@ void OGLWidget::initializeGL()
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 }
 
-void OGLWidget::resizeGL(int w, int h)
+void OGLWidget::resizeGL(int width, int height)
 {
     // Update projection matrix and other size related settings:
     //m_projection.setToIdentity();
     //m_projection.perspective(45.0f, w / float(h), 0.01f, 100.0f);
     //...
+
+    int side = qMin(width, height);
+    //glViewport((width - side) / 2, (height - side) / 2, side, side);
+    //glViewport(1000, 0, side, side);
+
+    float worldWidth = width/side;
+    float worldHeight= height/side;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-worldWidth, +worldWidth, -worldHeight, +worldHeight, 1.0, 3.0);
+    //glOrtho(-10, +10, -1, +1, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 void OGLWidget::paintGL()
 {
-    // Clear
-    glClear(GL_COLOR_BUFFER_BIT);
 
     // Draw the scene:
     //QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glLoadIdentity();
     glBlendColor(1, 1, 1, 1);
+    glTranslatef(0, 0, -2);
     GLfloat vVertices[] = {0.0f, 0.5f, 0.0f,
                           -0.5f, -0.5f, 0.0f,
                            0.5f, -0.5f, 0.0f};
