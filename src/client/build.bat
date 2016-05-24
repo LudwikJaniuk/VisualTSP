91e2d35a4555b9b640048afea740b9c72a2924ce
Felix Almay <almay@kth.se>
2016-05-24 16:41:29 +0200

    Update Win build scripts

--- src/client/build.bat
+++ src/client/build.bat
@@ -1,1 +1,1 @@
-cl /Od /wd4715 /MDd /EHsc -D_WIN32_WINNT=0x0501 /I C:\local\boost_1_60_0/ main.cpp /link /LIBPATH:C:\local\boost_1_60_0\lib64-msvc-14.0
+cl /wd4715 /MDd /EHsc -D_WIN32_WINNT=0x0501 /I C:\local\boost_1_60_0/ /I ../common main.cpp ../common/common.cpp /link /LIBPATH:C:\local\boost_1_60_0\lib64-msvc-14.0
